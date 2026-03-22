ca0e79602315a83eb473a80fe25d8a5d3f7f7237
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-22 23:29:39 +0530

    Array basics

---
+++ src/arrays/PassingInFunctions.java
@@ -0,0 +1,14 @@
+package arrays;
+import java.util.Arrays;
+public class PassingInFunctions {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+    }
+    static void change(int[] arr){
+        arr[0] = 12;
+        arr[4] = 33;
+        System.out.println(Arrays.toString(arr));
+    }
+}
