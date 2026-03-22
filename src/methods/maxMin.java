fc96f997c8826c78db29a5d4133d19c3bdd7c365
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-22 19:41:08 +0530

    Adding files to methods`

---
+++ src/methods/maxMin.java
@@ -0,0 +1,32 @@
+package methods;
+
+import java.util.Scanner;
+public class maxMin {
+    static void main() {
+    max();
+    min();
+    }
+    static void max(){
+        System.out.print("Enter two nums :");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        if (x>y)
+            System.out.println("Max element : " + x);
+
+        else
+            System.out.println("Max element : " + y);
+
+    }
+    static void min() {
+        System.out.print("Enter two nums : ");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        if (x < y)
+            System.out.println("Min element : " + x);
+
+        else
+            System.out.println("Min element : " + y);
+    }
+}
---
+++ src/methods/vote.java
@@ -0,0 +1,18 @@
+package methods;
+
+import java.util.Scanner;
+
+public class vote {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        int a = input.nextInt();
+        System.out.println(eligibility(a));
+    }
+    static String eligibility(int age){
+        if (age >= 18)
+            return "Eligible to methods.vote";
+        else
+            return "Not eligible to methods.vote";
+    }
+}
