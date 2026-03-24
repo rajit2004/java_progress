ab577ffc779b08d94e1ee64249c69c91600455f6
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-25 01:19:46 +0530

    new files

---
+++ src/searching_sorting/boolSearch.java
@@ -0,0 +1,19 @@
+package searching_sorting;
+
+public class boolSearch {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int target = 5;
+
+        System.out.println(find(array , target));
+    }
+    static boolean find(int[] arr , int target){
+        if(arr.length == 0 )
+            return false;              // if array is empty
+        for (int element : arr) {
+            if (element == target)
+                return true;
+        }
+        return false;          // not found case
+    }
+}
---
+++ src/searching_sorting/searchNretrunEle.java
@@ -0,0 +1,19 @@
+package searching_sorting;
+
+public class searchNretrunEle {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int target = 5;
+
+        System.out.println(find(array , target));
+    }
+    static int find(int[] arr , int target){
+        if(arr.length == 0 )
+            return -1;              // if array is empty
+        for (int element : arr) {
+            if (element == target)
+                return target;
+        }
+        return -1;          // not found case
+    }
+}
