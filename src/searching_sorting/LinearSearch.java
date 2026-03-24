5bd35d0b12abb8467387e68bff1db0537f102a7c
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-25 00:55:52 +0530

    new files

--- src/searching_sorting/LinearSearch.java
+++ src/searching_sorting/LinearSearch.java
@@ -1,4 +1,36 @@
 package searching_sorting;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class LinearSearch {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int n = in.nextInt();
+        String[] array = new String[n];
+
+        for (int i = 0; i< array.length ; i++){
+            System.out.print("Enter the element at index " + i + " : ");
+            array[i] = in.next();
+        }
+        System.out.println("Entered array : " + Arrays.toString(array));
+
+        System.out.print("Enter the target word : ");
+        String x = in.next();
+        System.out.println(linearsearch(array , x));
+    }
+
+    static String linearsearch(String[] arr, String target) {
+        if (arr.length == 0)
+            return "Empty!";
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(target)) {
+                return "Found at index " + i;
+            }
+        }
+        return "Not found!";
+    }
 }
