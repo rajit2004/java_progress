f22a37e87d6ff3c79b55ca8ee69001bc30811250
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-26 02:18:42 +0530

    new files

---
+++ src/searching_sorting/LeetCode_1295.java
@@ -0,0 +1,27 @@
+// 1295. Find Numbers with Even Number of Digits
+// (Given an array nums of integers, return how many of them contain an even number of digits.)
+
+package searching_sorting;
+
+public class LeetCode_1295 {
+    public static void main(String[] args) {
+
+        int[] arr = {23, -500, 78, 102, -34, 5612, 9, -111, 4210, 173};
+
+        System.out.println(sol(arr));
+
+    }
+    static int sol(int[] arr){
+        int n = 0;
+        for(int ele : arr){
+            int len;
+            if(ele == 0)
+                len = 1;
+            else
+                len = (int)Math.log10(Math.abs(ele)) + 1;
+            if(len % 2 == 0)
+                n++;
+        }
+        return n;
+    }
+}
--- src/searching_sorting/MaxIn2dArray.java
+++ src/searching_sorting/MaxIn2dArray.java
@@ -1,4 +1,34 @@
 package searching_sorting;
 
+import java.util.Arrays;
+
 public class MaxIn2dArray {
+    public static void main(String[] args) {
+        int[][] nums = {{10, -3, 25}, {7, 14}, {0, -9, 8, 21}, {5}, {32, -11, 6, 19, 4}, {17, 2}};
+
+        System.out.println("Array : " + Arrays.deepToString(nums));
+        System.out.println(max(nums));
+        System.out.println(min(nums));
+
+    }
+    static String max(int[][] arr){
+        int max = arr[0][0];
+        for(int[] row : arr){
+            for(int ele : row){
+                if(ele > max)
+                    max = ele;
+            }
+        }
+        return "Max element : " + max;
+    }
+    static String min(int[][] arr){
+        int min = arr[0][0];
+        for(int[] row : arr){
+            for(int ele : row){
+                if(ele < min)
+                    min = ele;
+            }
+        }
+        return "Min element : " + min;
+    }
 }
