e6966afc3940c018629c5b6d2034baf70c75d522
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-26 19:26:12 +0530

    new files

--- src/searching_sorting/BinarySearchAscending.java
+++ src/searching_sorting/BinarySearchAscending.java
@@ -1,6 +1,8 @@
 package searching_sorting;
 
-public class BinarySearch {
+//binary search for ascending order elements in array
+
+public class BinarySearchAscending {
     public static void main(String[] args) {
         int[] array = {1, 3, 5, 7, 9, 12, 15, 18, 21, 25, 30, 34, 39, 42, 47, 53, 58, 64, 71, 80};
         System.out.println(binarySearch(array , 30));
---
+++ src/searching_sorting/OrderAgnosticBinarySearch.java
@@ -0,0 +1,9 @@
+//works on both ascending and descending order
+//enhanced version of binary search
+package searching_sorting;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
