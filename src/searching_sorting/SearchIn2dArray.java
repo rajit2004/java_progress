7e3428f032e15201f9dfec1e37d2499e2d821ea8
Ranesh Rajit <raneshrajit@gmail.com>
2026-03-29 21:50:03 +0530

    New Files!

--- src/searching_sorting/SearchIn2dArray.java
+++ src/searching_sorting/SearchIn2dArray.java
@@ -6,7 +6,13 @@ import java.util.Scanner;
 public class SearchIn2dArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[][] arr = {{1,2,3},{4,5,6,7,8},{9,11,53,12,87,20,43},{69},{35,61,88,97}};
+        int[][] arr = {{3, -1, 7, 2, -9, 14},
+                {-5, 8, 0, -2, 11, -13, 6},
+                {15, -3, 4, 9, -7, 16, -10, 1},
+                {-8, 17, -4},
+                {18, -6, 5, 12, -11, 19, -14, 20, 21},
+                {13, -15, 22, -16, 23},
+                {-18, 24, -19, 25, -20, 26}};
         System.out.println("Array : " + Arrays.deepToString(arr));
         System.out.print("Enter elements to search : ");
         int target = in.nextInt();
