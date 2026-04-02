7b461db3b5749c07fc8a784ab88987daf8536e20
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-02 19:46:53 +0530

    New Files!

--- src/LeetCode/LeetCode_.java
+++ src/LeetCode/LeetCode_.java
@@ -2,7 +2,25 @@ package LeetCode;
 
 public class LeetCode_ {
     public static void main(String[] args) {
-
+        int[] nums = {2,3,5,9,14,16,18};
+        int target = 1;
+        System.out.println(ceiling(nums,target));
     }
-    static void fn(){}
+    static int ceiling(int[] arr , int target){
+        int st = 0;
+        int end = arr.length-1;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(target==arr[mid]){
+                return arr[mid];
+            }
+            if(target<arr[mid]){
+                end = mid-1;
+            }
+            else {
+                st = mid + 1;
+            }
+        }
+        return arr[st];
+    }
 }
