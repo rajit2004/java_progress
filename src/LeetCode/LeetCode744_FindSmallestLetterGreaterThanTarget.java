6889c08c8bf51868ddf43b91258ba324b0c1cbcc
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-02 23:03:39 +0530

    Solution to new questions!

--- src/LeetCode/LeetCode744_FindSmallestLetterGreaterThanTarget.java
+++ src/LeetCode/LeetCode744_FindSmallestLetterGreaterThanTarget.java
@@ -2,6 +2,28 @@ package LeetCode;
 
 public class LeetCode744_FindSmallestLetterGreaterThanTarget {
     public static void main(String[] args) {
+        char[] letters = {'a','c','e','g','i','k','m','o','q','s','u','w','y'};
+        char target = 'z';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+    static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length-1;
 
+        while(start<=end){
+
+            int mid = start + (end- start)/2;
+
+            if(target<letters[mid])
+                end = mid -1;
+            else
+                start = mid + 1;
+        }
+
+        // if target is greater than or equal to all elements
+        if(start == letters.length){
+            return letters[0];
+        }
+        return letters[start];
     }
 }
