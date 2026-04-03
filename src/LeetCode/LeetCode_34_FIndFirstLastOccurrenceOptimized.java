20f343b741e6a421ed5e3c0fdb6deeec558c8d38
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-03 09:20:19 +0530

    New LeetCode questions!

--- src/LeetCode/LeetCode_34_FIndFirstLastOccurrenceOptimized.java
+++ src/LeetCode/LeetCode_34_FIndFirstLastOccurrenceOptimized.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class LeetCode_34_FIndFirstLastOccurrenceOptimized {
     public static void main(String[] args) {
-        int[] nums = {6, 7, 7, 8, 8, 9};
+        int[] nums = {6,6,7,7,7,7,7,7, 7, 7, 8, 8, 9};
         int target = 7;
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
