c6e7acc672fbe3974335d395e4711bd60e0f32f6
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-04 00:43:18 +0530

    New LeetCode questions!

--- src/LeetCode/LeetCode_852_PeakIndexInMountainArrayOptimized.java
+++ src/LeetCode/LeetCode_852_PeakIndexInMountainArrayOptimized.java
@@ -3,8 +3,22 @@ package LeetCode;
 public class LeetCode_852_PeakIndexInMountainArrayOptimized {
     public static void main(String[] args) {
         int[] mountain_array = {1, 3, 7, 12, 18, 25, 21, 16, 10, 5, 2};
+        System.out.println(ansIndex(mountain_array));
     }
     static int ansIndex(int[] arr){
+        int st = 0;
+        int end = arr.length-1;
 
+        while (st<end){
+            int mid = st + (end-st)/2;
+
+            if(arr[mid]>arr[mid+1])         //descending part of the array
+                end = mid;
+            else                            //ascending part of the array
+                st = mid+1;
+        }
+        if(st==end)
+            return st;          // peak found
+        return -1;              // not found
     }
 }
