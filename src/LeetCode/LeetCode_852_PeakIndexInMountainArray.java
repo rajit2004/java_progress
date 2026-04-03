a442b4e4b458b0a2d2257df696bee2d395a9d89a
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-03 23:37:25 +0530

    New LeetCode questions!

--- src/LeetCode/LeetCode_852_PeakIndexInMountainArray.java
+++
@@ -1,7 +0,0 @@
-package LeetCode;
-
-public class LeetCode_852_PeakIndexInMountainArray {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/LeetCode/LeetCode_852_PeakIndexInMountainArrayBruteForce.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class LeetCode_852_PeakIndexInMountainArrayBruteForce {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 7, 12, 18, 25, 21, 16, 10, 5, 2};
+        System.out.println(maxEleIndex(array));
+    }
+    static int maxEleIndex(int[] arr){
+        int max = arr[0];
+        int index = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
