7eeedf212bec49db76d34866f521ad22d580df31
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-04 13:51:19 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_33_SearchInRotatedSortedArray.java
+++ src/LeetCode/LeetCode_33_SearchInRotatedSortedArray.java
@@ -2,6 +2,10 @@ package LeetCode;
 
 public class LeetCode_33_SearchInRotatedSortedArray {
     public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+    }
+    static int search(int[] nums, int target){
 
     }
 }
