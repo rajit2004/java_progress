6f07cb70272b631aac3b94f1995f3fb1c1599029
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-06 01:10:18 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized.java
+++ src/LeetCode/LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized.java
@@ -1,16 +1,74 @@
 package LeetCode;
 
-/*
-we try to keep it optimized in this
-time complexity should be O(log n)
-try to minimize crashing on edge cases
-in case of no rotation we ensure no crash
- */
-
 public class LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized {
+
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 0;
+        System.out.println(search(nums, target));
     }
 
+    // Find pivot (index of largest element)
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Case 1: mid is pivot
+            if (mid < end && arr[mid] > arr[mid + 1])
+                return mid;
+
+            // Case 2: mid-1 is pivot
+            if (mid > start && arr[mid] < arr[mid - 1])
+                return mid - 1;
+
+            // Decide direction
+//            figure out which half contains the answer and move there
+
+            if (arr[mid] >= arr[start])         //answer in right half
+                start = mid + 1;
+            else                                //answer in right half
+                end = mid - 1;
+        }
+        return -1; // no rotation
+    }
+
+    // Standard binary search
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+
+    // Main search function
+    static int search(int[] arr, int target) {
+        int pivot = findPivot(arr);
+
+        // Case: no rotation
+        if (pivot == -1)
+//            just do normal binary search
+            return binarySearch(arr, target, 0, arr.length - 1);
+
+//        if pivot is found we have two ascending sorted arrays
+
+        // Case: pivot is target
+        if (arr[pivot] == target)
+            return pivot;
+
+        // Decide which side to search
+        if (target >= arr[0])
+            return binarySearch(arr, target, 0, pivot - 1);
+
+        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+    }
 }
