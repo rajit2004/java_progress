9dacdfedde2fa579c34ccb1c7aab8b2298f2b103
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-05 22:08:19 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized.java
+++ src/LeetCode/LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized.java
@@ -1,8 +1,16 @@
 package LeetCode;
 
+/*
+we try to keep it optimized in this
+time complexity should be O(log n)
+try to minimize crashing on edge cases
+in case of no rotation we ensure no crash
+ */
+
 public class LeetCode_33_SearchInRotatedSortedArray_ActuallyOptimized {
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 0;
     }
+
 }
