4dee2317ff2c5589832bed4d13a25c2bc614deb4
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-07 00:06:39 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_217_ContainsDuplicate.java
+++ src/LeetCode/LeetCode_217_ContainsDuplicate.java
@@ -1,4 +1,17 @@
 package LeetCode;
 
 public class LeetCode_217_ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+    static boolean containsDuplicate(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] == arr[j])
+                    return true;
+            }
+        }
+        return false;
+    }
 }
