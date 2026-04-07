85193573300f30205cfb93d05433ec546efbfbfe
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-08 00:03:46 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_258_SumOfDigitsInNumber_BruteForce.java
+++ src/LeetCode/LeetCode_258_SumOfDigitsInNumber_BruteForce.java
@@ -3,5 +3,18 @@ package LeetCode;
 public class LeetCode_258_SumOfDigitsInNumber_BruteForce {
     public static void main(String[] args) {
         int num = 38;
+        System.out.println(addDigits(num));
+    }
+    static int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
+
