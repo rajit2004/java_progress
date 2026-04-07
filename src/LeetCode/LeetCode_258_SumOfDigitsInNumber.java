c8cebd25f7fce845336a816ed1327e43140da643
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-08 00:00:38 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_258_SumOfDigitsInNumber.java
+++ src/LeetCode/LeetCode_258_SumOfDigitsInNumber.java
@@ -1,4 +1,14 @@
 package LeetCode;
 
 public class LeetCode_258_SumOfDigitsInNumber {
+
+    public static int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
+    }
+
+    public static void main(String[] args) {
+        int num = 38; // test input
+        System.out.println(addDigits(num)); // expected output: 2
+    }
 }
