c2697aefd9daad6a1b689c0c6c8a262a20c97a6d
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-09 00:24:08 +0530

    Solutions to new LeetCode questions!

---
+++ src/LeetCode/LeetCode_410_SplitArrayLargestSum.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+/*
+Given an integer array nums and an integer k, split nums into k non-empty subarrays
+such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+*/
+
+public class LeetCode_410_SplitArrayLargestSum {
+    public static void main(String[] args) {
+
+    }
+}
