640f692b64a4e5d6f765996f7888c96d9b370035
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-08 21:24:04 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_35_SearchInsertPosition.java
+++ src/LeetCode/LeetCode_35_SearchInsertPosition.java
@@ -1,9 +1,32 @@
 package LeetCode;
 
-
+/*
+Given a sorted array of distinct integers and a target value,
+ return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+*/
 
 public class LeetCode_35_SearchInsertPosition {
     public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println(searchInsert(nums,target));
+    }
 
+    static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
     }
 }
