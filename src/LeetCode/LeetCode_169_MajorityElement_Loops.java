7aec327c8679b124e6ca6d5292b853ef022355ab
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-12 22:57:42 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_169_MajorityElement_Loops.java
+++ src/LeetCode/LeetCode_169_MajorityElement_Loops.java
@@ -7,7 +7,6 @@ public class LeetCode_169_MajorityElement_Loops {
         int[] arr = {2,2,1,1,1,2,2};
         System.out.println(majorityElement(arr));
         System.out.println(majorityElement2(arr));
-
     }
     static int majorityElement(int[] nums) {
         int n = nums.length;
@@ -16,16 +15,12 @@ public class LeetCode_169_MajorityElement_Loops {
             int count = 0;
 
             for(int j = 0; j < n; j++){
-                if(nums[j] == nums[i]){
+                if(nums[j] == nums[i])
                     count++;
-                }
             }
-
-            if(count > n / 2){
+            if(count > n / 2)
                 return nums[i];
-            }
         }
-
         return -1;
     }
     static int majorityElement2(int[] nums) {
@@ -35,21 +30,15 @@ public class LeetCode_169_MajorityElement_Loops {
         while(i < n){
             int count = 0;
             int j = 0;
-
             while(j < n){
-                if(nums[j] == nums[i]){
+                if(nums[j] == nums[i])
                     count++;
-                }
                 j++;
             }
-
-            if(count > n / 2){
+            if(count > n / 2)
                 return nums[i];
-            }
-
             i++;
         }
-
         return -1;
     }
 }
