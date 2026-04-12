5cd2140a62f724e70f7aec9f6e7514b242bd8979
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-13 00:38:39 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_66_PlusOne.java
+++ src/LeetCode/LeetCode_66_PlusOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LeetCode_66_PlusOne {
     public static void main(String[] args) {
-        int[] num = {9};
+        int[] num = {9,9,9};
         System.out.println(Arrays.toString(increment(num)));
     }
     static int[] increment(int[] arr) {
@@ -13,10 +13,11 @@ public class LeetCode_66_PlusOne {
                 arr[i]++;      // no carry needed
                 return arr;
             }
-            arr[i] = 0;        // set to 0 and carry continues
+//            else part:
+            arr[i] = 0;        // if last digit is more than 0 then set it to 0 and carry continues
         }
 
-        // if all digits were 9 → need new array
+        // If loop exits without returning → all digits were 9 → increase size
         int[] result = new int[arr.length + 1];
         result[0] = 1;
         return result;
