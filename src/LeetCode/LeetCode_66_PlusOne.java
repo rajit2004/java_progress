a51fe3f29b1dde8226131e0c6da7e117b8745fb0
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-12 23:30:58 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_66_PlusOne.java
+++ src/LeetCode/LeetCode_66_PlusOne.java
@@ -1,7 +1,17 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class LeetCode_66_PlusOne {
     public static void main(String[] args) {
-
+        int[] num = {1,2,3};
+        System.out.println(Arrays.toString(increment(num)));
+    }
+    static int[] increment(int[] arr){
+        int last = arr[arr.length-1] = arr[arr.length-1] + 1;
+        if(last>9){
+            int num1 = last%10;
+        }
+        return arr;
     }
 }
---
+++ src/basics/modulus.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class modulus {
+    public static void main(String[] args) {
+        int num = 10;
+        int q = num%10;
+        System.out.println(q);
+    }
+}
