158e0bf33c76978c72bfe7a025314232637d1782
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-14 00:00:51 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_41.java
+++ src/LeetCode/LeetCode_41.java
@@ -4,4 +4,35 @@ public class LeetCode_41 {
     public static void main(String[] args) {
 
     }
+
+    static int firstMissingPositive(int[] nums) {
+        int i = 0;
+
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] > 0 && nums[i] <= nums.length
+                    && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // for missing num
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
+
