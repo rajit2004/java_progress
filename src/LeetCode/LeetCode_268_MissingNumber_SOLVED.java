3b7f99391bcf95c6f315ab9661dc068dff23cb59
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-14 01:28:39 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_268_MissingNumber_SOLVED.java
+++ src/LeetCode/LeetCode_268_MissingNumber_SOLVED.java
@@ -1,4 +1,40 @@
 package LeetCode;
 
 public class LeetCode_268_MissingNumber_SOLVED {
+    public static void main(String[] args) {
+        int[] nums = {1,2};
+        System.out.println(missingNumber(nums));
+    }
+
+    static void swap(int[] arr , int num1 , int num2){
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+    }
+
+    static int missingNumber(int[] arr){
+//        first cycle sort :
+        int i = 0;
+
+        while (i<arr.length){
+            int correctIndex = arr[i];
+
+            if(arr[i]< arr.length && arr[i] != arr[correctIndex])
+                swap(arr,i,correctIndex);
+            else
+                i++;
+        }
+
+//        find missing number :
+        int ans = 0;
+
+        for(i = 0 ; i< arr.length ; i++){
+
+//            case 1: n in array
+            if(arr[i] != i)
+               return i;
+        }
+//        case 2 : n not in array
+        return arr.length;
+    }
 }
--- src/sorting/CycleSort_0toN.java
+++ src/sorting/CycleSort_0toN.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CycleSort_0toN {
     public static void main(String[] args) {
-        int[] nums = {3,5,0,2,1,4};
+        int[] nums = {4,3,2,0,1};
         System.out.println(Arrays.toString(cyclicSort(nums)));
     }
     static int[] cyclicSort(int[] arr) {
