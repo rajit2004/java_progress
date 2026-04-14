621cf7d405926c24fa3839966cb764424fec0cc6
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-14 23:05:54 +0530

    Cycle Sort Technique with Duplicate Entries!

--- src/LeetCode/LeetCode_448_FindAllNumsDisappearedFromArray.java
+++ src/LeetCode/LeetCode_448_FindAllNumsDisappearedFromArray.java
@@ -5,12 +5,12 @@ Given an array nums of n integers where nums[i] is in the range [1, n],
  return an array of all the integers in the range [1, n] that do not appear in nums.
 */
 
-
+import java.util.Arrays;
 
 public class LeetCode_448_FindAllNumsDisappearedFromArray {
     public static void main(String[] args) {
         int[] nums = {4,3,2,7,8,2,3,1};
-        System.out.println();
+        System.out.println(Arrays.toString(missingNum(nums)));
     }
     static void swap(int[] arr , int index1 , int index2){
         int temp = arr[index1];
