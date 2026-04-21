56811246fefc13a90933aeb47331ff95420ecf52
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-21 23:11:21 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_242_ALTERNATIVE.java
+++ src/LeetCode/LeetCode_242_ALTERNATIVE.java
@@ -1,4 +1,19 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class LeetCode_242_ALTERNATIVE {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    static boolean isAnagram(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
 }
