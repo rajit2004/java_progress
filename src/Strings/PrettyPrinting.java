a4cf8da0ba3499ea162837fa1645383998ab45f8
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-23 01:43:44 +0530

    Pretty Printing!

--- src/Strings/PrettyPrinting.java
+++ src/Strings/PrettyPrinting.java
@@ -1,5 +1,22 @@
 package Strings;
 
 public class PrettyPrinting {
+    public static void main(String[] args) {
+        float n = 761.126456789f;
+        System.out.printf("The formatted number is : %.2f" , n);
+        System.out.println();
+//        printf is used to print in formatted manner
+//        % is used to add placeholder
+//        .nf represents the num of decimal values we wanna print after the .
+//        let us assume %.3f => this prints 3 digits after the decimal
+//        it also rounds off the number to n places like in the above example
+        System.out.println(Math.PI);
+        System.out.printf("pi till 3 decimal places = %.3f" , Math.PI);
+        System.out.println();
 
+//        for strings
+        String name = "Ranesh";
+        String hobby = "coding";
+        System.out.printf("Hello I'm %s and my hobby is %s" , name , hobby);
+    }
 }
