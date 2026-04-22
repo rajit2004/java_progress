cd3d0f7e274779f523e1688fe890086e8d82c679
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-22 22:17:14 +0530

    Solutions to new LeetCode questions!

--- src/LeetCode/LeetCode_263_ALTERNATIVE.java
+++ src/LeetCode/LeetCode_263_ALTERNATIVE.java
@@ -2,6 +2,22 @@ package LeetCode;
 
 public class LeetCode_263_ALTERNATIVE {
     public static void main(String[] args) {
-
+        int n = 12;
+        System.out.println(isUgly(n));
+    }
+    static boolean isUgly(int n) {
+        while(true){
+            if(n==0)
+                return false;
+            else if(n%2==0)
+                n/=2;
+            else if(n%3==0)
+                n/=3;
+            else if(n%5==0)
+                n/=5;
+            else
+                break;
+        }
+        return n==1;
     }
 }
