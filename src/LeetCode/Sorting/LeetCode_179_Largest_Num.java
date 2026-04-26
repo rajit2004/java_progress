03af6ce9981acbe7117b60a101156dd1acbcd52b
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-27 00:13:41 +0530

    Solution to new LeetCode problem!

--- src/LeetCode/Sorting/LeetCode_179_Largest_Num.java
+++ src/LeetCode/Sorting/LeetCode_179_Largest_Num.java
@@ -1,8 +1,31 @@
 package LeetCode.Sorting;
 
+import java.util.Arrays;
+
 public class LeetCode_179_Largest_Num {
     public static void main(String[] args) {
         int[] num = {3,30,34,5,9};
+        System.out.println(largestNumber(num));
     }
+    static String largestNumber(int[] nums) {
+        // Convert int array to String array
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
 
+        // Custom sort
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        // Edge case: if largest is "0"
+        if (arr[0].equals("0")) return "0";
+
+        // Build result
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
+
+        return result.toString();
+    }
 }
