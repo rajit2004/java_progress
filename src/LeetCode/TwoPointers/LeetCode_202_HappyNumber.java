bfd9d6b6229d90d838cebbab6cf03e09125efce6
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-29 20:34:16 +0530

    Solution to new LeetCode Problem!

---
+++ src/LeetCode/TwoPointers/LeetCode_202_HappyNumber.java
@@ -0,0 +1,31 @@
+package LeetCode.TwoPointers;
+
+public class LeetCode_202_HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+
+    }
+    static public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    static int getNext(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
