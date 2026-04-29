97301a53face9e7f9e7649d17dfe37ba2fb75709
Ranesh Rajit <raneshrajit@gmail.com>
2026-04-29 11:51:24 +0530

    Solution to new LeetCode Problem!

--- src/LeetCode/BinarySearch/LeetCode_374_GuessNum.java
+++ src/LeetCode/BinarySearch/LeetCode_374_GuessNum.java
@@ -1,7 +1,44 @@
 package LeetCode.BinarySearch;
 
 public class LeetCode_374_GuessNum {
-    public static void main(String[] args) {
 
+    // Simulated picked number (hidden in real LeetCode)
+    static int picked = 6;          // guesses
+
+    public static void main(String[] args) {
+        int n = 6;         // actual number
+        int result = guessNumber(n);
+        System.out.println("Guessed number: " + result);
+    }
+
+    static int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+
+            if (res == 0) {
+                return mid;
+            } else if (res == -1) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1; // should never reach here
+    }
+
+    // Simulated guess API
+    static int guess(int num) {
+        if (num == picked) {
+            return 0;
+        } else if (num > picked) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 }
