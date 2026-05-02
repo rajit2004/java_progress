bea7cdc0f7b184309f8cf98ccef1b845d5b9706e
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-03 00:05:37 +0530

    Solution to new LeetCode problem!

--- src/LeetCode/Strings/LeetCode_20_ValidParentheses.java
+++ src/LeetCode/Strings/LeetCode_20_ValidParentheses.java
@@ -1,6 +1,14 @@
 package LeetCode.Strings;
 
-public class LeetCode_29_ValidParentheses {
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+*/
+
+public class LeetCode_20_ValidParentheses {
     public static void main(String[] args) {
         String s = "()[]{}";
         System.out.println(isValid(s));
