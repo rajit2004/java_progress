4c006e224b71db396a507d4820847ba49c6cf75c
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-03 19:24:10 +0530

    Solution to new LeetCode problem!

--- src/LeetCode/Strings/LeetCode_796_RotatedStringAlt.java
+++ src/LeetCode/Strings/LeetCode_796_RotatedStringAlt.java
@@ -2,6 +2,17 @@ package LeetCode.Strings;
 
 public class LeetCode_796_RotatedStringAlt {
     public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println(rotateString(s,goal));
+    }
+    static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
 
+        for (int i = 0; i < s.length(); i++) {
+            s = s.substring(1) + s.charAt(0);
+            if (s.equals(goal)) return true;
+        }
+        return false;
     }
 }
