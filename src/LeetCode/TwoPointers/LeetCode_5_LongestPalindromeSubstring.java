6a6af446f773cfcaa23eda84dd3ac93a45ba29f4
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-08 22:43:11 +0530

    Solved the Longest substring problem using two pointers method!

---
+++ src/LeetCode/TwoPointers/LeetCode_5_LongestPalindromeSubstring.java
@@ -0,0 +1,56 @@
+package LeetCode.TwoPointers;
+
+public class LeetCode_5_LongestPalindromeSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(longestPalindrome(s));
+    }
+    static String longestPalindrome(String s) {
+
+        // Edge case
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        // Try every character as center
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            int len1 = expandFromCenter(s, i, i);
+
+            // Even length palindrome
+            int len2 = expandFromCenter(s, i, i + 1);
+
+            // Take maximum length
+            int len = Math.max(len1, len2);
+
+            // Update answer
+            if (len > end - start) {
+
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    // Helper function
+    static int expandFromCenter(String s, int left, int right) {
+
+        // Expand while valid palindrome
+        while (left >= 0 &&
+                right < s.length() &&
+                s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        // Length of palindrome
+        return right - left - 1;
+    }
+}
