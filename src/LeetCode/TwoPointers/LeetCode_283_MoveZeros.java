64313b8a1dc3774ea2e8fe9ee0bf4e2f9bf48bfe
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-10 14:22:09 +0530

    Solved the Move Zeros problem using two pointers method

--- src/LeetCode/TwoPointers/LeetCode_283_MoveZeros.java
+++ src/LeetCode/TwoPointers/LeetCode_283_MoveZeros.java
@@ -5,11 +5,27 @@ Given an integer array nums, move all 0's to the end of it while
 maintaining the relative order of the non-zero elements.
 */
 
+import java.util.Arrays;
+
 public class LeetCode_283_MoveZeros {
     public static void main(String[] args) {
         int[] arr = {1,0,2,12,3};
+        System.out.println(Arrays.toString(moveZeros(arr)));
     }
-    static void moveZeros(int[] nums){
-
+    static int[] moveZeros(int[] nums){
+        int insPos = 0;
+//        we move all the non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=0){
+                nums[insPos] = nums[i];
+                insPos++;
+            }
+        }
+//        Now we fill the remaining positions with 0s
+        while(insPos < nums.length){
+            nums[insPos] = 0;
+            insPos++;
+        }
+        return nums;
     }
 }
