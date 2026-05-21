060d8cad2c81f0c563bc755e095b9a1af071553d
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-21 16:35:25 +0530

    palindrome using methods

---
+++ src/Strings/PalindromeMethod.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class PalindromeMethod {
+    public static void main(String[] args) {
+        System.out.println(ispalindrome("RaCeCaR"));
+    }
+    static boolean ispalindrome(String str){
+
+        if(str == null || str.isEmpty())
+            return true;
+
+        str = str.toLowerCase();
+
+        for (int i = 0 ; i <= (str.length()-1)/2 ; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if(start != end)
+                return false;
+        }
+        return true;
+    }
+}
