774d906894c89925d8fc9f9adbfae6970665bff6
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-21 17:05:55 +0530

    Solution to leetcode 709 using the in-built tolower method !

---
+++ src/LeetCode/Strings/LeetCode_709_ToLowerCase.java
@@ -0,0 +1,11 @@
+package LeetCode.Strings;
+
+public class LeetCode_709_ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("APPLE"));
+    }
+    static String toLowerCase(String s){
+        s = s.toLowerCase();
+        return s;
+    }
+}
--- src/Questions/Patterns/pattern1.java
+++ src/Questions/Patterns/pattern1.java
@@ -1,4 +1,7 @@
 package Questions.Patterns;
 
 public class pattern1 {
+    public static void main(String[] args) {
+
+    }
 }
