ece03b711881a86f75cf197cdb8e7a8c39d01f99
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-23 09:59:41 +0530

    Solution to LeetCode length of last word problem by using the approach of skipping trails from the last and then counting with runtime = 0ms!

--- src/LeetCode/Strings/LeetCode_58_lengthOfLastWord.java
+++ src/LeetCode/Strings/LeetCode_58_lengthOfLastWord.java
@@ -1,7 +1,27 @@
-package LeetCode;
+package LeetCode.Strings;
+
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 public class LeetCode_58_lengthOfLastWord {
     public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+    static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
 
+        // Step 1: skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Step 2: count last word length
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
