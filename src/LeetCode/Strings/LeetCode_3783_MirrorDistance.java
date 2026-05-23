ed674092ac10d45660fcf1ec8776659ca1bc30f4
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-23 09:50:12 +0530

    Solving LeetCode mirror dist. problem using the given definition from the problem statement!

--- src/LeetCode/Strings/LeetCode_3783_MirrorDistance.java
+++ src/LeetCode/Strings/LeetCode_3783_MirrorDistance.java
@@ -1,7 +1,20 @@
-package LeetCode;
+package LeetCode.Strings;
 
 public class LeetCode_3783_MirrorDistance {
     public static void main(String[] args) {
+        int digit = 25;
+        System.out.println(mirrorDistance(digit));
+    }
+    static int mirrorDistance(int n) {
+        int original = n;
+        int reversed = 0;
 
+        // Reverse the number using basic math
+        while (n > 0) {
+            int digit = n % 10;          // get last digit
+            reversed = reversed * 10 + digit; // build reversed number
+            n = n / 10;                 // remove last digit
+        }
+        return Math.abs(original - reversed);
     }
 }
