1e46b9521a97493fb57387a27a8f116a39a693d6
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-26 23:14:36 +0530

    Solution to leetcode Number of Special characters {3120} using traversal and comparison with runtime = 1ms!

--- src/LeetCode/Strings/Leetcode_3120_NumSpecialChara.java
+++ src/LeetCode/Strings/Leetcode_3120_NumSpecialChara.java
@@ -1,15 +1,26 @@
-class Solution {
-    public int numberOfSpecialChars(String word) {
-        
+package LeetCode.Strings;
+
+/*
+You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+
+Return the number of special letters in word.
+*/
+
+public class Leetcode_3120_NumSpecialChara {
+    public static void main(String[] args) {
+        String word = "aaAbcBC";
+        System.out.println(numberOfSpecialChars(word));
+    }
+    static int numberOfSpecialChars(String word){
         // Store lowercase letters
         boolean[] lower = new boolean[26];
-        
+
         // Store uppercase letters
         boolean[] upper = new boolean[26];
 
         // Traverse the string
         for (char ch : word.toCharArray()) {
-            
+
             if (Character.isLowerCase(ch)) {
                 lower[ch - 'a'] = true;
             } else {
