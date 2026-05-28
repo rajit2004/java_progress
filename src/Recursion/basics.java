26d1d52d4b050e35f25fa54f6436518203a731c4
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-28 23:32:35 +0530

    Recursion basic ideas

--- src/Recursion/basics.java
+++ src/Recursion/basics.java
@@ -3,7 +3,43 @@ package Recursion;
 public class basics {
     public static void main(String[] args) {
 
-//        in recursion, we call the function inside of a function .
+//        in recursion, we call the function inside another function .
+//        msg();
+      /*
+        msg2();
+        msg3();
+        msg4();
+        */
+
+//        so instead of calling various the msg fn again n again why don't we just
+
+//        solution ???
+
+//        call one fn inside of another fn
+//        and now we just call single msg fn which will call all the remaining fns.
+        msg();
 
     }
+//    ex:
+
+    static void msg(){
+        System.out.println("hello world!");
+        msg1();
+    }
+    static void msg1(){
+        System.out.println("hello world!");
+        msg2();
+    }
+    static void msg2(){
+        System.out.println("hello world!");
+        msg3();
+    }
+    static void msg3(){
+        System.out.println("hello world!");
+        msg4();
+    }
+    static void msg4(){
+        System.out.println("hello world!");
+    }
+
 }
