6cc1f439b5514364571303c0c068c6580a449046
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-31 00:20:36 +0530

    Recursion question practice

---
+++ src/Recursion/numsFromN_to_1.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class numsFromN_to_1 {
+    public static void main(String[] args) {
+        numPrint(10);
+    }
+    static void numPrint(int n){
+        if(n<1)
+            return;
+        System.out.println(n);
+        numPrint(n - 1);
+    }
+}
