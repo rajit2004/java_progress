b7db81d1deb23f870ce35f02f79979f351564a40
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-31 01:00:58 +0530

    Recursion question practice

--- src/Recursion/factorial.java
+++ src/Recursion/factorial.java
@@ -2,6 +2,33 @@ package Recursion;
 
 public class factorial {
     public static void main(String[] args) {
-
+//        System.out.println(factRec(-2));
+        System.out.println(factRec(6));
+        System.out.println(factRec(0));
     }
+    static int fact(int n){
+
+        if(n<0)
+            throw new IllegalArgumentException("Number must be positive !");
+
+       int prod = 1;
+       while(n > 0) {
+           prod *= n;
+           n--;
+       }
+       return prod;
+    }
+
+//    factorial using recursion :
+    static int factRec(int n){
+
+        if(n<0)
+            throw new IllegalArgumentException("Number must be positive !");
+
+        if(n == 0)
+            return 1;
+        return n * factRec(n-1);
+    }
+
+
 }
