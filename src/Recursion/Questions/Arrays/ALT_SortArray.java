6b6cfaaca70d9863e618ab8a5b57acd02370c5ae
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-31 23:18:37 +0530

    Recursive approach to see if the array is sorted

---
+++ src/Recursion/Questions/Arrays/ALT_SortArray.java
@@ -0,0 +1,18 @@
+package Recursion.Questions.Arrays;
+
+public class ALT_SortArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,8,12};
+    }
+    static boolean sorted(int[] arr , int index){
+
+//        base condition : when we have reached the last index -> the array was sorted
+        if(index == arr.length - 1)
+            return true;
+
+//        recursive call : first we check whether the iTh ele is smaller than the i+1 ele or not then we recursively call the fn to check the array
+//                              for i+1 index and so on, and we use 'AND' to get the result
+        return arr[index] < arr[index+1] && sorted(arr , index + 1);
+    }
+
+}
