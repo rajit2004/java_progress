9b5194bb27ab397ef6b4ed1ec417b0e60199be69
Ranesh Rajit <raneshrajit@gmail.com>
2026-05-31 23:32:06 +0530

    Recursive approach to see if the array is sorted

--- src/Recursion/Questions/Arrays/ALT_SortArray.java
+++ src/Recursion/Questions/Arrays/ALT_SortArray.java
@@ -3,6 +3,7 @@ package Recursion.Questions.Arrays;
 public class ALT_SortArray {
     public static void main(String[] args) {
         int[] arr = {1,2,4,8,12};
+        System.out.println(sorted(arr, 0));
     }
     static boolean sorted(int[] arr , int index){
 
---
+++ src/Recursion/Questions/Arrays/LinearSearch.java
@@ -0,0 +1,24 @@
+//package Recursion.Questions.Arrays;
+//
+//public class LinearSearch {
+//    public static void main(String[] args) {
+//        int[] arr = {9,4,7,1,5,23,6};
+//        int target = 71;
+//        System.out.println(search(arr , target));
+//    }
+//
+////    iterative approach :
+//    static boolean search(int[] arr , int target){
+//        for(int ele : arr){
+//            if(ele == target)
+//                return true;
+//        }
+//        return false;
+//    }
+//
+////    recursive approach :
+//    static boolean linsearch(int[] arr , int target){
+//        if(arr[])
+//    }
+//
+//}
