612dddab1e03bbb1ca73edeed98e99c622087791
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-04 16:53:27 +0530

    Solution to LeetCode problem Waviness of Nums in range using helper fn + recursion with runtime = 34ms

--- src/LeetCode/Numbers/LeetCode_3751_TotalWavinessOfNNum.java
+++ src/LeetCode/Numbers/LeetCode_3751_TotalWavinessOfNNum.java
@@ -14,10 +14,30 @@ Return the total sum of waviness for all numbers in the range [num1, num2].
 
 public class LeetCode_3751_TotalWavinessOfNNum {
     public static void main(String[] args) {
-        int num1= 120;
-        int num2 = 130;
+        int n = 120;
+        int m = 130;
+        System.out.println(totalWaviness(n,m));
+    }
+    static int helper(int num){
+        String s = String.valueOf(num);
+        int peak = 0;
+        int valley = 0;
+        for(int i = 1 ; i <= s.length()-2 ; i++) {              // s.length-2 bcoz peak or valley can't be the last or first element
+            if (s.charAt(i-1) < s.charAt(i) && s.charAt(i) > s.charAt(i + 1))
+                peak++;
+            if(s.charAt(i - 1) > s.charAt(i) && s.charAt(i) < s.charAt(i+1))
+                valley++;
+        }
+        return peak + valley;
     }
     static int totalWaviness(int num1, int num2){
-        return 0;
+//        base condition :
+        if(num1 > num2)
+            return 0;
+        if(num1 == num2)
+            return helper(num1);
+
+// recursive call:
+        return helper(num1) + totalWaviness(num1+1 , num2);
     }
 }
