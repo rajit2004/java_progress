ce16dfcb4d0bc5ec36bf4be2c7d745f29165f9f5
Ranesh Rajit <raneshrajit@gmail.com>
2026-06-04 16:52:00 +0530

    Solution to LeetCode problem Waviness of Nums in range using helper fn + recursion with runtime = 34ms

---
+++ src/LeetCode/Numbers/LeetCode_3751_TotalWavinessOfNNum.java
@@ -0,0 +1,23 @@
+package LeetCode.Numbers;
+
+/*
+You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+
+The waviness of a number is defined as the total count of its peaks and valleys:
+
+A digit is a peak if it is strictly greater than both of its immediate neighbors.
+A digit is a valley if it is strictly less than both of its immediate neighbors.
+The first and last digits of a number cannot be peaks or valleys.
+Any number with fewer than 3 digits has a waviness of 0.
+Return the total sum of waviness for all numbers in the range [num1, num2].
+*/
+
+public class LeetCode_3751_TotalWavinessOfNNum {
+    public static void main(String[] args) {
+        int num1= 120;
+        int num2 = 130;
+    }
+    static int totalWaviness(int num1, int num2){
+        return 0;
+    }
+}
